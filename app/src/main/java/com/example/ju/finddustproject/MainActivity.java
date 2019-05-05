94219c33b8474c01cf8845ad786f5407307391aa
kangraemin <ee4787@naver.com>
2019-05-05 20:43:32 +0900

    detailCityDustInfo updates

--- app/src/main/java/com/example/ju/finddustproject/MainActivity.java
+++ app/src/main/java/com/example/ju/finddustproject/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.ju.finddustproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button SearchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        
-        //윤희중
-        //강래민 주석a
-        //강래민 대답해
+        SearchButton = findViewById(R.id.SearchFineDustBySidoName);
+        SearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SidoName.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
